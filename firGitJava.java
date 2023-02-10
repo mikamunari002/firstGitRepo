76e9e676360aee8f69a87e8e9716fc67c1400edc
Mika <mikamunari002@gmail.com>
2023-02-10 18:36:17 -0500

    created a function called addNums

---
+++ firGitJava.java
@@ -0,0 +1,23 @@
+public class firGitJava {
+
+	public static void main(String[] args){
+		System.out.println(addNums());
+
+
+
+
+	}
+
+	public static int addNums(){
+		int x = 2;
+		int y= 3;
+
+		return x + y;
+
+
+	}
+
+
+
+
+}
